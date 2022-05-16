5381877f2939c6f88e5fba9f6b165287a2423c28
mertc <mert.can58@hotmail.de>
2022-05-16 17:03:38 +0200

    Meilenstein 2 done

--- src/main/java/htw/berlin/webtech/demo/web/api/ThingRestController.java
+++ src/main/java/htw/berlin/webtech/demo/web/api/ThingRestController.java
@@ -1,11 +1,13 @@
 package htw.berlin.webtech.demo.web.api;
 
+import htw.berlin.webtech.demo.persistence.ThingEntity;
 import htw.berlin.webtech.demo.persistence.ThingsRepository;
 import htw.berlin.webtech.demo.service.ThingService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,12 +16,34 @@ public class ThingRestController {
 
     private final ThingService thingService;
 
-    public ThingRestController(ThingService thingService){
+    public ThingRestController(ThingService thingService) {
         this.thingService = thingService;
     }
 
     @GetMapping(path = "/api/v1/things")
-    public ResponseEntity<List<Thing>> fetchThing(){
+    public ResponseEntity<List<Thing>> fetchThing() {
         return ResponseEntity.ok(thingService.findall());
+    }
+    @GetMapping(path = "/api/v1/things/{id}")
+    public ResponseEntity<Thing> fetchThingById(@PathVariable Long id){
+        var thing = thingService.findById(id);
+        return thing != null? ResponseEntity.ok(thing) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(path = "/api/v1/things")
+    public ResponseEntity<Void> createThing(@RequestBody ThingCreate request) throws URISyntaxException {
+        var thing =thingService.create(request);
+        URI uri = new URI("/api/v1/things/" + thing.getId());
+        return ResponseEntity.created(uri).build();
+}
+    @PutMapping(path = "/api/v1/things/{id}")
+    public ResponseEntity<Thing> updateThing(@PathVariable Long id,@RequestBody ThingCreate request) {
+        var thing =thingService.update(id,request);
+        return thing != null? ResponseEntity.ok(thing) : ResponseEntity.notFound().build();
+    }
+    @DeleteMapping(path = "/api/v1/things/{id}")
+    public ResponseEntity<Void> deleteThing(@PathVariable Long id){
+        boolean succesful = thingService.deleteById(id);
+        return succesful? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 }
